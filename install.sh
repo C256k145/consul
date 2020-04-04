ed7fc407af828a41674a9aa91845b1ccac400032
c256k145 <noremackientz@gmail.com>
2020-04-04 13:31:45 -0500

    initial commit

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo wget -P /usr/local/bin https://releases.hashicorp.com/consul/0.8.0/consul_0.8.0_linux_amd64.zip
+sudo apt install unzip
+sudo unzip /usr/local/bin/consul_0.8.0_linux_amd64.zip
+sudo rm -rf /usr/local/bin/consul_0.8.0_linux_amd64.zip
